6e8837aa0340aba77126df55d4ae8107087a8756
Andrey <andr.poluehktov@yandex.ru>
2022-12-01 09:05:41 +0300

    change eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    parser: 'babel-eslint'
   },
   plugins: ['vue'],
   rules: {
